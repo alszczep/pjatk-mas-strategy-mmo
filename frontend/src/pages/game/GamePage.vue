f122b9f4da6bd20dff83307453e2909aeb6b9729
alszczep <al.szczepura@gmail.com>
2024-05-25 22:58:55 +0200

    Add game route, Cleanup routes

---
+++ frontend/src/pages/game/GamePage.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-layout>
+    <router-view name="NavBar" />
+    <router-view />
+  </v-layout>
+</template>
--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -4,24 +4,48 @@ import {
   type RouteLocationNormalized,
 } from "vue-router"
 
+import NavBar from "./components/common/NavBar.vue"
 import LoginPage from "./pages/LoginPage.vue"
 import RegisterPage from "./pages/register/RegisterPage.vue"
-import VillagePage from "./pages/village/VillagePage.vue"
+import GamePage from "./pages/game/GamePage.vue"
+import VillagePage from "./pages/game/village/VillagePage.vue"
 import { isLoggedIn } from "./auth/isLoggedIn"
 
 export const routes = {
-  login: "/",
-  register: "/register",
-  village: "/village",
-}
+  login: {
+    index: "/",
+  },
+  register: {
+    index: "/register",
+  },
+  game: {
+    index: "/game",
+    village: "/game/village",
+  },
+} as const
+
+export const getLastRoutePart = (route: `${string}/${string}`) =>
+  route.split("/").at(-1) ?? ""
 
 const routesComponents = [
-  { path: routes.login, component: LoginPage },
+  { path: routes.login.index, component: LoginPage },
   {
-    path: routes.register,
+    path: routes.register.index,
     component: RegisterPage,
   },
-  { path: routes.village, component: VillagePage },
+  {
+    path: routes.game.index,
+    component: GamePage,
+    children: [
+      {
+        path: getLastRoutePart(routes.game.village),
+        components: {
+          default: VillagePage,
+          NavBar,
+        },
+      },
+    ],
+  },
 ]
 
 export const router = createRouter({
@@ -30,17 +54,17 @@ export const router = createRouter({
 })
 
 router.beforeEach((to) => {
-  const pathsWithoutAuth = [routes.login, routes.register]
+  const pathsWithoutAuth: string[] = [routes.login.index, routes.register.index]
   const isPathWithoutAuth = (p: RouteLocationNormalized) =>
     pathsWithoutAuth.includes(p.path)
 
   if (isPathWithoutAuth(to)) {
     if (isLoggedIn()) {
-      return routes.village
+      return routes.game.village
     }
   } else {
     if (!isLoggedIn()) {
-      return routes.login
+      return routes.login.index
     }
   }
 })
